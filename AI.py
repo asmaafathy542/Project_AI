976e49b5d30f93752e26496133b152bfdfe79c81
asmaafathy542 <129214546+asmaafathy542@users.noreply.github.com>
2025-12-12 15:38:12 +0200

    Add files via upload

--- AI.py
+++ AI.py
@@ -1,14 +1,17 @@
-
 import streamlit as st
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-
+import pickle
 from ai_utils import clean_text
 
 model = load_model("sms_lstm_model.h5")
 
+with open("tokenizer.pickle", "rb") as handle:
+    tokenizer = pickle.load(handle)
+
+max_len = 100
+
 def predict_text(text):
-    # preprocessing: clean, tokenize, pad
     clean = clean_text(text)
     seq = tokenizer.texts_to_sequences([clean])
     seq_pad = pad_sequences(seq, maxlen=max_len, padding='post', truncating='post')
@@ -19,6 +22,9 @@ def predict_text(text):
 st.title("SMS Spam Detection")
 user_input = st.text_area("Enter your message:")
 if st.button("Predict"):
-    result = predict_text(user_input)
-    st.write(result)
+    if user_input.strip() != "":
+        result = predict_text(user_input)
+        st.write(result)
+    else:
+        st.write("Please enter a message to predict.")
 
